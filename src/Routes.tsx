f98b374b615f3e327b910e8710dc4d321d6b3a93
Gustavo <gustavonikov@gmail.com>
2021-06-26 05:07:25 -0300

    update: Adding AuthContext on Routes

--- src/Routes.tsx
+++ src/Routes.tsx
@@ -1,14 +1,17 @@
-import { BrowserRouter, Switch, Route } from 'react-router-dom'
+import { BrowserRouter, Route } from 'react-router-dom'
 import { Home } from './page/Home'
 import { NewRoom } from './page/NewRoom'
 
+import { AuthContextProvider } from './contexts/AuthContext'
+
 export default function Routes() {
+
     return (
         <BrowserRouter>
-            <Switch>
+            <AuthContextProvider>
                 <Route path="/" exact component={Home} />
                 <Route path="/rooms/new" component={NewRoom} />
-            </Switch>
+            </AuthContextProvider>
         </BrowserRouter>
     )
 }
